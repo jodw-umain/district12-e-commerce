87ddcef405a30a07dfb456509582f33f6e7ccfd8
Natalia Torres <natalia.torres@umain.com>
2025-10-13 15:15:20 +0200

    all variants with colors, add global colors

--- frontend/app/components/ui/button.tsx
+++ frontend/app/components/ui/button.tsx
@@ -5,19 +5,17 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow hover:bg-costume-hover",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/50",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+          "border border-foreground bg-background shadow-sm hover:border-custome-btn hover:text-custome-btn",
+        ghost: "border border-background hover:border-custome-btn hover:text-custome-btn",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -15,7 +15,6 @@ export default async function Page() {
         <p>{settings?.title || 'hero'}</p>
       <Button variant="default">default</Button>
        <Button variant="destructive">destructive</Button>
-        <Button variant="outline">outline</Button>
          <Button variant="secondary">secondary</Button>
           <Button variant="ghost">ghost</Button>
       </div>
