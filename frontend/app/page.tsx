243df678d1923a96580f7148fcf9c3bdcce71654
Natalia Torres <natalia.torres@umain.com>
2025-10-13 14:05:30 +0200

    display diferent button variants

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ import {Suspense} from 'react'
 import {AllProducts} from '@/app/components/Products'
 import {settingsQuery} from '@/sanity/lib/queries'
 import {sanityFetch} from '@/sanity/lib/live'
+import {Button} from '@/app/components/ui/button'
 
 export default async function Page() {
   const {data: settings} = await sanityFetch({
@@ -12,6 +13,11 @@ export default async function Page() {
     <>
       <div className="relative">
         <p>{settings?.title || 'hero'}</p>
+      <Button variant="default">default</Button>
+       <Button variant="destructive">destructive</Button>
+        <Button variant="outline">outline</Button>
+         <Button variant="secondary">secondary</Button>
+          <Button variant="ghost">ghost</Button>
       </div>
       <div className="border-t border-gray-100 bg-gray-50">
         <div className="container">
