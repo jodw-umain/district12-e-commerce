a2e36a4bebcf184c537c1b73e74fcabca9e17758
jodw-umain <235268637+jodw-umain@users.noreply.github.com>
2025-10-13 14:43:33 +0200

    feat(productCard): style produt card

---
+++ frontend/app/components/ProductCard.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import {Card, CardHeader, CardDescription, CardContent, CardTitle, CardFooter} from './ui/card'
+
+interface ProductCard {
+  image?: string
+  productTitle?: string
+  artist?: string
+  price?: string
+  category?: string
+}
+
+export default function ProductCard({image, productTitle, artist, price, category}: ProductCard) {
+  return (
+    <Card className="p-0 rounded-none border-none shadow-none">
+      <CardContent className="p-0">
+        {/* <Image src={`${image}`} fill alt={`Art made by ${artist}`} /> */}
+        <img src={`${image}`} />
+      </CardContent>
+      <CardHeader className="p-0 pt-4">
+        <CardTitle>
+          {productTitle}, {artist}
+        </CardTitle>
+        <CardDescription>{category}</CardDescription>
+      </CardHeader>
+      <CardFooter className="rounded-none p-0 pt-4">
+        <p>${price}</p>
+      </CardFooter>
+    </Card>
+  )
+}
