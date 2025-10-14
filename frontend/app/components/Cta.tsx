24f68421d3eb834a579c834603bda89328251a03
Natalia Torres <natalia.torres@umain.com>
2025-10-14 09:41:15 +0200

    added variant to button cta

--- frontend/app/components/Cta.tsx
+++ frontend/app/components/Cta.tsx
@@ -13,7 +13,7 @@ export default function CTA({block}: CtaProps) {
   return (
     <div className="container my-12">
       <div className="bg-gray-50 border border-gray-100 rounded-2xl max-w-3xl">
-        <div className="px-12 py-12 flex flex-col gap-6">
+        <div className="px-12 py-12 flex gap-6">
           <div className="max-w-xl flex flex-col gap-3">
             <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
               {block.heading}
@@ -26,7 +26,7 @@ export default function CTA({block}: CtaProps) {
               <ResolvedLink
                 link={block.link}
               >
-                <Button>{block.buttonText}</Button>
+                <Button variant={block.buttonVariant}>{block.buttonText}</Button>
               </ResolvedLink>
             </div>
           </Suspense>
