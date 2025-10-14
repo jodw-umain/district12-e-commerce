53c3b25567be269ebeb758773de79a4058498ca9
Natalia Torres <natalia.torres@umain.com>
2025-10-14 11:32:34 +0200

    delete buttons from /page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,6 @@ import {Suspense} from 'react'
 import {AllProducts} from '@/app/components/Products'
 import {settingsQuery} from '@/sanity/lib/queries'
 import {sanityFetch} from '@/sanity/lib/live'
-import {Button} from '@/app/components/ui/button'
 
 export default async function Page() {
   const {data: settings} = await sanityFetch({
@@ -13,10 +12,6 @@ export default async function Page() {
     <>
       <div className="relative">
         <p>{settings?.title || 'hero'}</p>
-      <Button variant="default">default</Button>
-       <Button variant="destructive">destructive</Button>
-         <Button variant="secondary">secondary</Button>
-          <Button variant="ghost">ghost</Button>
       </div>
       <div className="border-t border-gray-100 bg-gray-50">
         <div className="container">
