b434819b1e515b57c486e3ba200652bccfc3338b
Natalia Torres <natalia.torres@umain.com>
2025-10-15 15:36:20 +0200

    frontend component structure

---
+++ frontend/app/components/ProductDetails.tsx
@@ -0,0 +1,34 @@
+// import {ProductDetails} from '@/sanity.types'
+
+// type ProductDetailsProps = {
+//   block: HeroSection
+//   index: number
+// }
+
+import Image from 'next/image';
+import {Button} from '@/app/components/ui/button'
+
+export default function ProductDetails() {
+
+  return (
+    <section className="flex items-center">
+      <div className="container flex items-center justify-center py-6 px-2 sm:px-6">
+        <Image src="/placeholder.png" alt="Product image" width={200} height={200} />
+        <div>
+            <h1 className='text-4xl'>Product title</h1>
+            <h2 className='text-grey-500 text-xl'>Artist</h2>
+            <p>Description</p>
+            <p>price </p>
+            <p>category</p>
+            <div className='flex gap-2'>
+                   <p>Drawing</p>
+                    <p>Digital</p>
+            </div>
+            <Button/>
+           
+
+        </div>
+      </div>
+    </section>
+  )
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ import {Suspense} from 'react'
 import {AllProducts} from '@/app/components/Products'
 import {settingsQuery} from '@/sanity/lib/queries'
 import {sanityFetch} from '@/sanity/lib/live'
+import ProductDetails from './components/ProductDetails'
 
 export default async function Page() {
   const {data: settings} = await sanityFetch({
@@ -15,6 +16,7 @@ export default async function Page() {
       <div className="border-t border-gray-100 bg-gray-50">
         <div className="container">
           <aside className="py-12 sm:py-20">
+            <ProductDetails/>
             <Suspense>{await AllProducts()}</Suspense>
           </aside>
         </div>
