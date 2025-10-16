92b0b95c05d404c3596114ae2f05735f018e8489
jodw-umain <235268637+jodw-umain@users.noreply.github.com>
2025-10-16 16:01:56 +0200

    feat(productCarousel): add carousel to products block

--- frontend/app/components/ProductsBlock.tsx
+++ frontend/app/components/ProductsBlock.tsx
@@ -2,6 +2,9 @@ import {sanityFetch} from '@/sanity/lib/live'
 import ProductCard from './ProductCard'
 import {allProductsQuery} from '@/sanity/lib/queries'
 
+import {Carousel, CarouselContent, CarouselItem} from './ui/carousel'
+import {AllProductsQueryResult} from '@/sanity.types'
+
 export default async function ProductsBlock() {
   const {data} = await sanityFetch({query: allProductsQuery})
 
@@ -14,12 +17,23 @@ export default async function ProductsBlock() {
   }
 
   return (
-    <div>
-      {data.map((product: any) => (
-        <ProductCard key={product._id} product={product} />
-      ))}
-
-      <p>hello</p>
+    <div className="continer">
+      <h2 className="text-4xl">Title</h2>
+      <Carousel
+        opts={{
+          containScroll: false,
+          align: 'start',
+          dragFree: true,
+        }}
+      >
+        <CarouselContent>
+          {data.map((product) => (
+            <CarouselItem className="basis-1/2 md:basis-1/3 lg:basis-1/6" key={product._id}>
+              <ProductCard product={product} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </div>
   )
 }
