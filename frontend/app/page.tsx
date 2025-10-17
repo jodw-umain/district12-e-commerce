cf63f4c759955b4e6c09b7e1e529f9f09fbd2077
Natalia Torres <natalia.torres@umain.com>
2025-10-17 10:21:25 +0200

    lp m

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import {settingsQuery} from '@/sanity/lib/queries'
 import {sanityFetch} from '@/sanity/lib/live'
+import Hero from '@/app/components/Hero'
 
 export default async function Page() {
   const {data: settings} = await sanityFetch({
@@ -11,6 +12,7 @@ export default async function Page() {
       <div className="relative"></div>
       <div className="container">
         <aside className="py-12 sm:py-20">
+      
         </aside>
         <aside className="py-12 sm:py-20"></aside>
       </div>
