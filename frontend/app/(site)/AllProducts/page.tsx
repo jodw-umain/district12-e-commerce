dbec8cb75ec918935dd2a7014ff6f655cc58146f
yitianshen-glitch <yitian.shen@umain.com>
2025-10-21 11:47:19 +0200

    refactor: use productcard in productpage

--- frontend/app/(site)/AllProducts/page.tsx
+++ frontend/app/(site)/AllProducts/page.tsx
@@ -1,15 +1,26 @@
 import { client } from "@/sanity/lib/client";
 import { getAllProductsQuery } from "@/sanity/lib/queries";
 import Image from "next/image";
+import { urlForImage } from "@/sanity/lib/utils";
+import ProductCard from "@/app/components/ProductCard";
+import { sanityFetch } from "@/sanity/lib/live";
 
 export default async function AllProductsPage() {
-  const products = await client.fetch(getAllProductsQuery);
+  // const products = await client.fetch(getAllProductsQuery);
+  const products = await sanityFetch({ query: getAllProductsQuery });
 
   return (
     <section className="p-8">
       <h1 className="text-2xl font-bold mb-4">All Products</h1>
 
-      <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {/* <ProductCard product={products} /> */}
+      <ul className="grid">
+        {products.data.map((p) => (
+          <ProductCard key={p._id} product={p} />
+        ))}
+      </ul>
+
+      {/* <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {products.map((p: any) => (
           <li key={p._id}>
             <div className="relative aspect-square w-full overflow-hidden rounded-md">
@@ -24,7 +35,7 @@ export default async function AllProductsPage() {
             <p className="text-sm text-gray-600">${p.productPrice}</p>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </section>
   );
 }
