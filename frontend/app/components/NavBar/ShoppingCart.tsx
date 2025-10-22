96a483cc959f9c2f420dfc16598a7f161d975fcf
jodw-umain <235268637+jodw-umain@users.noreply.github.com>
2025-10-22 18:27:27 +0200

    feat(CartIconComponent): fetch data from localstorage

--- frontend/app/components/NavBar/ShoppingCart.tsx
+++ frontend/app/components/NavBar/ShoppingCart.tsx
@@ -1,14 +1,21 @@
-import Link from "next/link";
-import { ShoppingBag } from "lucide-react";
+'use client'
+
+import Link from 'next/link'
+import {ShoppingBag} from 'lucide-react'
+import {useCartStore} from '@/lib/stores/useCartStore'
+import useStore from '@/lib/hooks/useStore'
 
 export default function ShoppingCart() {
+  const totalItems = useStore(useCartStore, (state) => state.getTotalItems())
+
   return (
     <Link href="/shoppingcart" className="relative" aria-label="Shopping cart">
       <ShoppingBag className="w-5 h-5" />
-      <span className="absolute -top-2 -right-2 text-[10px] px-[6px] py-[2px] bg-black text-white rounded">
-        0
-      </span>
+      {totalItems !== undefined && totalItems > 0 && (
+        <span className="absolute -top-2 -right-2 text-[10px] px-[6px] py-[2px] bg-black text-white rounded">
+          {totalItems}
+        </span>
+      )}
     </Link>
-  );
+  )
 }
-
