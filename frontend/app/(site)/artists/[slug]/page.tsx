7835717a75ca74e54df35cc084a6678819b5b794
Natalia Torres <natalia.torres@umain.com>
2025-10-22 16:13:00 +0200

    styling for page

--- frontend/app/(site)/artists/[slug]/page.tsx
+++ frontend/app/(site)/artists/[slug]/page.tsx
@@ -1,8 +1,9 @@
 import {getProductsByArtistQuery} from '@/sanity/lib/queries'
 import {sanityFetch} from '@/sanity/lib/live'
 import type {GetProductsByArtistQueryResult} from '@/sanity.types'
-import {urlForImage} from '@/sanity/lib/utils'
 import Image from 'next/image'
+import {Button} from '@/app/components/ui/button'
+import Link from 'next/link'
 
 export default async function ArtistsPage({params}: {params: {slug: string}}) {
   const slug = params.slug
@@ -15,49 +16,65 @@ export default async function ArtistsPage({params}: {params: {slug: string}}) {
     return <div>No products found for {slug}</div>
   }
   const artist = data[0].author
-  // const artistImageUrl = urlForImage(artist?.picture)?.url() ?? ''
   const artistImageUrl = artist?.picture?.url ?? ''
 
   return (
-    <section className="p-8">
-      <div className="flex items-center justify-between py-6">
-        <h1>{artist?.authorName}</h1>
+    <section className="p-8 container">
+      <div className="sm:flex justify-between py-8 ">
+        <div className="flex flex-col">
+          <div className="items-center rounded-full w-40 h-40 overflow-hidden">
+            {artist?.picture?.url && (
+              <Image
+                src={artist.picture.url}
+                alt={artist.picture?.alt ?? 'Artist image'}
+                width={200}
+                height={200}
+                className="object-cover"
+              />
+            )}
+          </div>
+          <h1>{artist?.authorName}</h1>
+          <p>@{artist?.authorName}</p>
+          <Button className="mt-4">Contact</Button>
+        </div>
         <div className="items-center">
-          {artist?.picture?.url && (
-            <Image
-              src={artist.picture.url}
-              alt={artist.picture?.alt ?? 'Artist image'}
-              width={200}
-              height={200}
-              className="object-cover"
-            />
-          )}
+          <ul className="">
+            {data.map((p) => {
+              return (
+                <Link
+                  className="hover:text-brand underline transition-colors"
+                  href={`/products/${p.slug}`}
+                >
+                  <li key={p._id}>
+                    <div className="w-full h-full realtive">
+                      {p.picture?.url && (
+                        <Image
+                          src={p.picture.url}
+                          alt={p.picture?.alt ?? 'Product image'}
+                          width={500}
+                          height={200}
+                          className="object-cover"
+                        />
+                      )}
+                    </div>
+                    <h2 className="mt-2">{p.productName}</h2>
+                    <p className="text-gray-600">${p.productPrice}</p>
+                    {p.categories && p.categories.length > 0 && (
+                      <div className="flex flex-wrap gap-2 mt-2">
+                        {p.categories.map((cat) => (
+                          <p key={cat.slug} className="">
+                            {cat.title}
+                          </p>
+                        ))}
+                      </div>
+                    )}
+                  </li>
+                </Link>
+              )
+            })}
+          </ul>
         </div>
       </div>
-
-      <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {data.map((p) => {
-          // const productImageUrl = urlForImage(p.picture)?.url() ?? ''
-          return (
-            <li key={p._id}>
-              <div className="w-full h-64 relative ">
-                {/* {productImageUrl && ( */}
-                {p.picture?.url && (
-                  <Image
-                    // src={productImageUrl}
-                    src={p.picture.url}
-                    alt={p.picture?.alt ?? 'Product image'}
-                    className="rounded-md object-cover"
-                    fill
-                  />
-                )}
-              </div>
-              <h2 className="font-semibold mt-2">{p.productName}</h2>
-              <p className="text-sm text-gray-600">${p.productPrice}</p>
-            </li>
-          )
-        })}
-      </ul>
     </section>
   )
 }
