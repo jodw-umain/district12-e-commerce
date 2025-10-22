a7860ca8775dd7182ec9d71725d13c5c14bb2f9d
yitianshen-glitch <yitian.shen@umain.com>
2025-10-22 13:17:17 +0200

    change shoppingbag icon

--- frontend/app/components/NavBar/ShoppingCart.tsx
+++ frontend/app/components/NavBar/ShoppingCart.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
-import { ShoppingBag } from "lucide-react";
+import Image from "next/image"
+import ShoppingBag from "@/public/images/shoppingbag.svg";
 
 export default function ShoppingCart() {
   return (
     <Link href="/shoppingcart" className="relative" aria-label="Shopping cart">
-      <ShoppingBag className="w-5 h-5" />
-      <span className="absolute -top-2 -right-2 text-[10px] px-[6px] py-[2px] bg-black text-white rounded">
-        0
-      </span>
+      <Image src={ShoppingBag} alt="Shopping Bag" className="w-5 h-5" />
     </Link>
   );
 }
