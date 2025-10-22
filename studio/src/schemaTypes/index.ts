29cf19a407a3c8cb083d8c593298642cd8c70b3b
yitianshen-glitch <yitian.shen@umain.com>
2025-10-22 15:59:40 +0200

    move navbar and footer.ts from documents to singleton in index.ts

--- studio/src/schemaTypes/index.ts
+++ studio/src/schemaTypes/index.ts
@@ -13,8 +13,8 @@ import {artistCard} from './objects/artistCard'
 import {productsBlock} from './objects/productsBlock'
 import {productDetails} from './objects/productDetails'
 import {landingPage} from './singletons/landingPage'
-import navbar from './documents/navbar'
-import footer from './documents/footer'
+import navbar from './singletons/navbar'
+import footer from './singletons/footer'
 
 // Export an array of all the schema types.  This is used in the Sanity Studio configuration. https://www.sanity.io/docs/schema-types
 
@@ -22,8 +22,9 @@ export const schemaTypes = [
   // Singletons
   settings,
   landingPage,
-  // Documents
   navbar,
+  footer,
+  // Documents
   page,
   post,
   author,
