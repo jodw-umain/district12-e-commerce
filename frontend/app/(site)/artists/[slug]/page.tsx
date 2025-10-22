c9fe85a1e51d28ec1d91aa1d41864f0974df4af9
Natalia Torres <natalia.torres@umain.com>
2025-10-22 15:16:27 +0200

    display images

--- frontend/app/(site)/artists/[slug]/page.tsx
+++ frontend/app/(site)/artists/[slug]/page.tsx
@@ -15,28 +15,49 @@ export default async function ArtistsPage({params}: {params: {slug: string}}) {
     return <div>No products found for {slug}</div>
   }
   const artist = data[0].author
-  const artistImageUrl = urlForImage(artist?.picture)?.url() ?? ''
+  // const artistImageUrl = urlForImage(artist?.picture)?.url() ?? ''
+  const artistImageUrl = artist?.picture?.url ?? ''
 
   return (
     <section className="p-8">
-      <h1 className="text-2xl font-bold mb-4">{artist?.authorName}</h1>
-      {/* 
+      <div className="flex items-center justify-between py-6">
+        <h1>{artist?.authorName}</h1>
+        <div className="items-center">
+          {artist?.picture?.url && (
+            <Image
+              src={artist.picture.url}
+              alt={artist.picture?.alt ?? 'Artist image'}
+              width={200}
+              height={200}
+              className="object-cover"
+            />
+          )}
+        </div>
+      </div>
+
       <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {data.map((p) => (
-          <li key={p._id}>
-            <div className="relative aspect-square w-full overflow-hidden rounded-md">
-              <Image
-                src={prductImageUrl}
-                alt={p.picture.alt ?? 'Product image'}
-                className="rounded-md"
-                fill
-              />
-            </div>
-            <h2 className="font-semibold mt-2">{p.productName}</h2>
-            <p className="text-sm text-gray-600">${p.productPrice}</p>
-          </li>
-        ))}
-      </ul> */}
+        {data.map((p) => {
+          // const productImageUrl = urlForImage(p.picture)?.url() ?? ''
+          return (
+            <li key={p._id}>
+              <div className="w-full h-64 relative ">
+                {/* {productImageUrl && ( */}
+                {p.picture?.url && (
+                  <Image
+                    // src={productImageUrl}
+                    src={p.picture.url}
+                    alt={p.picture?.alt ?? 'Product image'}
+                    className="rounded-md object-cover"
+                    fill
+                  />
+                )}
+              </div>
+              <h2 className="font-semibold mt-2">{p.productName}</h2>
+              <p className="text-sm text-gray-600">${p.productPrice}</p>
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
