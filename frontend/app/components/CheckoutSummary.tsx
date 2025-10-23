5a8c0fceef3883b5345d03feb0ba07043808014e
jodw-umain <235268637+jodw-umain@users.noreply.github.com>
2025-10-23 13:34:32 +0200

    fix(checkoutSummary): removed styling from heading and add card component

--- frontend/app/components/CheckoutSummary.tsx
+++ frontend/app/components/CheckoutSummary.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import {useStore} from 'zustand'
 import {Button} from './ui/button'
 import {useEffect, useState} from 'react'
+import {Card, CardContent, CardFooter, CardHeader} from './ui/card'
 
 export default function CheckoutSummary() {
   const [isHydrated, setIsHydrated] = useState(false)
@@ -52,8 +53,8 @@ export default function CheckoutSummary() {
   }
 
   return (
-    <section className="p-8">
-      <h1 className="text-3xl font-bold mb-8">
+    <section className="container mb-4">
+      <h1 className="mb-8">
         Your cart <span className="text-gray-400">({totalItems})</span>
       </h1>
 
@@ -65,24 +66,30 @@ export default function CheckoutSummary() {
           </Link>
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 md:space-x-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 sm:space-x-4">
           <ul className="flex flex-col space-y-4">
             {cartItems.map((item) => (
-              <li key={item._id} className="border p-4 rounded-md gap-3 md:flex">
-                <Image
-                  src={`${urlForImage(item.picture)}`}
-                  width={200}
-                  height={200}
-                  priority
-                  alt="product"
-                  className="object-fit"
-                />
-                <div className="w-full flex flex-col justify-between">
-                  <h2 className="text-lg font-semibold">{item.productName}</h2>
-                  <h3>{item.author?.authorName}</h3>
-                  <p className="text-sm text-gray-600">${item.productPrice}</p>
-                  <div className="inline-flex space-x-4 items-center justify-between">
-                    <div className="flex space-x-2 justify-center items-center jus">
+              <li key={item._id} className="sm:flex">
+                <Card className="w-fit">
+                  <CardContent>
+                    <Image
+                      src={`${urlForImage(item.picture)}`}
+                      width={200}
+                      height={200}
+                      priority
+                      alt="product"
+                      className="object-fit"
+                    />
+                  </CardContent>
+                  <CardHeader className="flex flex-row items-center justify-between">
+                    <div>
+                      <h2>{item.productName}</h2>
+                      <h3>{item.author?.authorName}</h3>
+                    </div>
+                    <p className="text-sm text-gray-600">${item.productPrice}</p>
+                  </CardHeader>
+                  <CardFooter className="flex justify-between">
+                    <div className="flex space-x-2 items-center">
                       <Button onClick={() => handleDecreaseQuantity(item._id)}>
                         <span>-</span>
                       </Button>
@@ -95,8 +102,8 @@ export default function CheckoutSummary() {
                     <Button onClick={() => handleRemoveProduct(item._id)}>
                       <span>delete</span>
                     </Button>
-                  </div>
-                </div>
+                  </CardFooter>
+                </Card>
               </li>
             ))}
           </ul>
