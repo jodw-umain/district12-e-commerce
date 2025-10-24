1a8d870a7fda8cc6f57630d7ab7aee72b525a33e
Natalia Torres <natalia.torres@umain.com>
2025-10-24 11:05:55 +0200

    change shopByCategory section

--- frontend/app/components/FilterSection/FilterSection.tsx
+++ frontend/app/components/FilterSection/FilterSection.tsx
@@ -1,6 +1,7 @@
 import {sanityFetch} from '@/sanity/lib/live'
 import {getLandingPage} from '@/sanity/lib/queries'
 import Link from 'next/link'
+import {Button} from '../ui/button'
 
 export default async function FilterSection() {
   const {data: landing} = await sanityFetch({query: getLandingPage})
@@ -10,20 +11,20 @@ export default async function FilterSection() {
   return (
     <>
       {filterSection && (
-       <section className="text-black py-12 px-2 sm:px-6 flex justify-center">
+        <section className="py-12 px-2 sm:px-6 flex justify-center">
           <div className="flex items-center flex-wrap gap-3">
             <h2 className="text-2xl mb-0 mr-6">{filterSection.title}</h2>
             {filterSection.filters.map((filter) => (
               <Link
                 key={filter.title}
-                className="px-4 py-2 text-white bg-black rounded hover:bg-gray-300 transition"
+                className="px-4 py-2 rounded pill-button"
                 href={`categories/${filter.slug}`}
               >
-                {filter.title}
+                <Button variant="secondary">{filter.title}</Button>
               </Link>
             ))}
           </div>
-       </section>
+        </section>
       )}
     </>
   )
